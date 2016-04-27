28be518462ba6201572212df01c1d44c20fe70ef
Rachid Yahiaoui <ryahiaoui@ORL-5CB3410DK4.orleans.inra.fr>
2016-04-27 10:12:14 +0200

    CoreseInfer Generator

---
+++ src/main/java/corese/Main.java
@@ -0,0 +1,322 @@
+package corese;
+
+/**
+ *
+ * @author ryahiaoui
+ */
+
+import fr.inria.acacia.corese.api.IDatatype;
+import fr.inria.acacia.corese.exceptions.EngineException;
+import fr.inria.edelweiss.kgram.core.Mapping;
+import fr.inria.edelweiss.kgram.core.Mappings;
+import fr.inria.edelweiss.kgraph.core.Graph;
+import fr.inria.edelweiss.kgraph.query.QueryProcess;
+import fr.inria.edelweiss.kgraph.rule.RuleEngine;
+import fr.inria.edelweiss.kgtool.load.Load;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Main {
+      
+        private static volatile Main _instance = null;
+                
+        private static Graph        g   ;
+        private static Load         ld  ;
+        
+        private static RuleEngine   re  ;
+      
+        int flushCount = 5000  ;
+        
+        
+        private Main(){}
+
+        public static Main getInstance( List<String> filesToLoad, boolean entailment ) {
+                if (_instance == null) {
+                        synchronized (Main.class){
+                                if (_instance == null) {
+                                        _instance = new Main();
+                                        initialize(filesToLoad , entailment);
+                                }
+                        }
+                }
+                return _instance;
+        }
+      
+        private static void initialize( List<String> filesToLoad, boolean entailment  ) {
+          
+            try {
+                g = Graph.create(entailment);             
+                ld = Load.create(g);
+                     for(String file : filesToLoad ){
+                           ld.load(file);
+                }
+            }
+            catch (Exception ex) {
+                    ex.printStackTrace();
+            }
+        }
+
+      private String toStringDataType ( Mapping m, String value ) {
+           IDatatype dt = (IDatatype) m.getValue(value);
+           if(dt == null) return null ;
+           dt.intValue()        ;
+           dt.doubleValue()     ;
+           dt.booleanValue()    ;
+           dt.stringValue()     ; 
+           dt.getLabel()        ;
+           dt.getDatatypeURI()  ;
+           dt.getLang()         ;
+           dt.isURI()           ;
+           dt.isLiteral()       ;
+           dt.isBlank()         ;
+           return dt.getLabel() ;
+        }
+      
+        public void genericRequest( String request    ,                                     
+                                    String outputFile , 
+                                    int fragment      , 
+                                    int numRequest    ,
+                                    boolean ilv 
+                                  ) throws IOException  {
+              
+                List<String> variables = getVariables(request)  ;
+                QueryProcess exec      = QueryProcess.create(g) ;
+                Mappings map           = null                   ;
+                
+                try {
+                        map = exec.query(request);
+                }catch (EngineException e){
+                        System.err.println("catch : " + e);
+                }
+
+                List<String> lines = new ArrayList<>();
+                                
+                String res     = ""    ;
+                int count      = 0     ;
+                int loop       = 0     ;
+                
+                String currentFile     ;
+                
+                currentFile =  getCurrentFile(outputFile, numRequest, fragment , loop ) ;
+                Writer.checkFile( currentFile );
+                 
+                for (Mapping m : map) {
+                    
+                    for(String variable : variables ) {
+                       
+                        String dt =  toStringDataType(m, variable);
+                        
+                        if(dt == null ) continue ;
+                        
+                        if( dt.toLowerCase().startsWith("http://") ) {
+                           res += "<" + URLEncoder.encode(dt) + "> ";
+                        }
+                        else {
+                           res += "\"" + dt.replaceAll("\"", "'")
+                                           .replaceAll("\n", " ") + "\" ";   
+                        }
+                    }
+                    /* Ignore Blank node or literal values  */
+                    if(!ilv ) {
+                        count ++;                    
+                        lines.add( res + " . " ) ;
+                    }
+                    else if(res.startsWith("<http://") ) {
+                         count ++;                    
+                         lines.add( res + " . " ) ;
+                      }
+
+                    if(fragment != 0 && count % fragment == 0  ) {
+                       if(!lines.isEmpty()) {
+                          Writer.writeTextFile(lines, currentFile ) ;
+                          lines.clear();
+                          currentFile =  getCurrentFile(outputFile, numRequest, fragment , ++loop ) ;
+                          Writer.checkFile( currentFile );
+                       }
+                    }
+                    
+                    if( lines.size() % flushCount == 0 ) {                     
+                        Writer.writeTextFile(lines, currentFile ) ;
+                        lines.clear() ;
+                    }
+                                        
+                    res = "" ;
+                }
+                
+                if(!lines.isEmpty()) {
+                   Writer.writeTextFile(lines,  currentFile) ;
+                   lines.clear() ;                    
+                }
+                
+                /*
+                   print XML Result
+                   ResultFormat xmlOutput = ResultFormat.create(map);
+                   System.out.println(f); 
+                
+                   print Turtle Result
+                   TripleFormat tfOutput = TripleFormat.create(g, true);
+                   System.out.println(f);
+                */
+                
+                /* Delete last file if empty */
+                if(Files.lines( Paths.get(currentFile)).count() == 0 ) {
+                   Paths.get(currentFile).toFile().delete();
+                } 
+        }
+
+        public static List<String> getVariables( String sparqlQuery ) {
+        
+            List<String> variables = new ArrayList<>();
+            
+            String[] tokens  = sparqlQuery.replaceAll("\\s+", " ")
+                                          .replaceAll(Pattern.quote("("), " ")
+                                          .replaceAll(Pattern.quote(")"), " ")
+                                          .replaceAll(Pattern.quote("{"), " ")
+                                          .replaceAll(Pattern.quote("}"), " ")
+                                          .replaceAll(Pattern.quote(","), " ")
+                                          .replaceAll(Pattern.quote(";"), " ")
+                                          .replaceAll(Pattern.quote("."), " ")
+                                          .split(" ");
+
+            for(String token : tokens ) {
+               if(token.equalsIgnoreCase("AS")) {
+                   variables.remove(variables.size() - 1 );
+                   continue ;
+               }
+               
+               if(token.startsWith("?") & !variables.contains(token )) {
+                    variables.add(token);
+                }
+               else if( token.equalsIgnoreCase("where") ) break ;
+            }
+            return variables ;
+        }
+        
+        public static boolean isSelectQuery ( String query ) {
+            return query.trim()
+                        .replaceAll("\\s+", " ")
+                        .toLowerCase()
+                        .contains("select "
+                        .toLowerCase());
+        }
+        
+        private static String getCurrentFile(  String outFile , 
+                                               int numRequest , 
+                                               int fragment   ,
+                                               int loop    )  {
+            if(fragment <=0 ) {
+              return outFile ; 
+            }
+            if(Files.isDirectory(Paths.get(outFile)) ) {
+                  if(outFile.endsWith("/")) {
+                   return outFile+numRequest+"."+loop ; }
+                   else {
+                   return outFile+"/"+numRequest+"."+loop ; }
+            }
+            else {
+                if(fragment > 0 ) {
+                  return outFile+"."+loop ; }
+                else {
+                  return outFile ; }
+            }
+            
+       } 
+        
+        public static void main(String[] args) throws IOException {
+
+            if( args.length < 6 ) {
+                System.out.println(" Nombre paramètres incomplet ! ");
+                return ;
+            }
+            
+            List<String> owls         = new ArrayList<>();
+            List<String> nts          = new ArrayList<>();
+            List<String> queries      = new ArrayList<>();
+            List<String> outs         = new ArrayList<>();
+            List<Integer> fragments   = new ArrayList<>();
+            List<String> ilvs         = new ArrayList<>();
+            boolean entailment        = false            ;
+
+            for ( int i = 0 ; i < args.length ; i++ ) {
+                
+                String token = args[i];
+                
+                switch(token) {
+                    
+                    case "-owl" :  owls.add(args[i+1])      ;        
+                                   break ;
+                    case "-nt"  :  nts.add(args[i+1])       ;
+                                   break ;
+                    case "-out" :  outs.add(args[i+1])      ;
+                                   break ;
+                    case "-q"   :  queries.add(args[i+1])   ; 
+                                   break ;
+                    case "-f"   :  fragments.add(Integer.parseInt(args[i+1])) ;
+                                   break ;
+                    case "-ilv" :  ilvs.add(args[i+1]) ;
+                                   break ;
+                    case "-e"   :  entailment = true; ;
+                                   break ;
+                }
+            }
+            
+            System.out.println(" Owls : ");
+            owls.stream().forEach( e -> System.out.println("  " + e )) ;
+            System.out.println(" nts  : ");
+            nts.stream().forEach( e -> System.out.println("  " + e ))  ;
+                   
+            if( owls.isEmpty() || nts.isEmpty() ) {
+                 System.out.println(" owl or nt parameter is empty !! ") ;
+                 return ;
+            }
+            
+            if( ( queries.isEmpty() ) ) {
+                 System.out.println("  Error nbr parameters !! ");
+                 return ;
+            }
+            if(  queries.size() != outs.size() || ( queries.size() != fragments.size() 
+                    || queries.size() != ilvs.size() )) {
+                 System.out.println(" Bad size List queries-outs-fragment !! ");
+                 return ;
+            }
+
+            List<String> entryFiles = new ArrayList<>() ; 
+            entryFiles.addAll(owls) ;
+            entryFiles.addAll(nts)  ;
+            
+            /* Load Graph */
+            Main instance = Main.getInstance(entryFiles , entailment);
+            
+            /* Travers Queries */
+               for(int i = 0; i< queries.size(); i++ ) {
+                   
+                 if(isSelectQuery(queries.get(i))) {
+                     
+                   System.out.println("-------------------------------------------") ;
+                   
+                   System.out.println(" + Executing query : " + queries.get(i) )     ;
+                   System.out.println(" + Fragment        :  "+fragments.get(i))     ;
+                   System.out.println(" + Out             :  "+outs.get(i))          ;
+              
+                   boolean ilv = ilvs.get(i).toLowerCase().equals("t") ;
+                       
+                   instance.genericRequest( queries.get(i)   , 
+                                            outs.get(i)      ,  
+                                            fragments.get(i) ,
+                                            i                ,
+                                            ilv ) ; 
+                   
+                   System.out.println("-------------------------------------------") ;
+
+                 }
+                 else {
+                   System.out.println(queries.get(i) + " \n " +
+                   " not supported yet. Only Select Queries for the moment ! ");
+                 }
+               }
+        }
+}
