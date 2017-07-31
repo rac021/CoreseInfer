8208a1f6726e0626c86ced32f3f73e56a8763f34
Yahiaoui Rachid <rac021@hotmail.com>
2017-07-31 17:15:18 +0200

    Update SparqlTemplate.java

--- src/main/java/corese/sparql/SparqlTemplate.java
+++ src/main/java/corese/sparql/SparqlTemplate.java
@@ -250,3 +250,27 @@ public class SparqlTemplate {
    
 }
 
+
+/*
+    # SPARQL Exemple
+    # SPARQL TEMPLATE QUERY 
+    # ONE VARIABLE PER LINE 
+      
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+      
+    SELECT 
+         #  ?infra
+         #  ?infraName
+    
+    WHERE  {
+
+           ......
+    }
+
+    GROUP BY 
+    
+          #  ?infra
+          #  ?infraName
+*/
+
